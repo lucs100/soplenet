7da82c839a34edeeb31d4410ee638200fbf4f2cb
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-07-15 00:59:51 -0400

    Implement backpropagation of error

--- SopleNet.py
+++ SopleNet.py
@@ -1,6 +1,8 @@
 import numpy as np
 import time
 
+np.set_printoptions(edgeitems=30, linewidth=100000)
+
 class InputSizeException(Exception):
     """Raised when the size of an input vector does not agree with the size of the input layer"""
     def __init__(self, input, realInput):
@@ -9,44 +11,85 @@ class InputSizeException(Exception):
 MATRIX_MODE = "C" # C for row-contiguous, F for column-contiguous
 
 def sigmoid(x):
+    """The sigmoid function, a 'squishification' function for activations."""
     return 1 / (1 + np.exp(-x))
 
+def dSigmoid(x):
+    """The derivative of the sigmoid function."""
+    return sigmoid(x) * (1-sigmoid(x)) 
+
 class NeuralNetwork4: #4-layer neural network
-    def __init__(self, inputNeuronsCount, hiddenNeurons1Count, hiddenNeurons2Count, outputNeuronsCount):
-        self.inputNeuronsCount = inputNeuronsCount
-        self.hiddenNeurons1Count = hiddenNeurons1Count
-        self.hiddenNeurons2Count = hiddenNeurons2Count
-        self.outputNeuronsCount = outputNeuronsCount
+    def __init__(self, inputNeuronCount, h1NeuronCount, h2NeuronCount, outputNeuronCount):
+        self.inputNeuronCount = inputNeuronCount
+        self.h1NeuronCount = h1NeuronCount
+        self.h2NeuronCount = h2NeuronCount
+        self.outputNeuronCount = outputNeuronCount
 
-        self.h1Weights = np.random.rand(hiddenNeurons1Count, inputNeuronsCount)
-        self.h1Biases = np.random.rand(hiddenNeurons1Count, 1)
-        self.h2Weights = np.random.rand(hiddenNeurons2Count, hiddenNeurons1Count)
-        self.h2Biases = np.random.rand(hiddenNeurons2Count, 1)
-        self.outputWeights = np.random.rand(outputNeuronsCount, hiddenNeurons2Count)
-        self.outputBiases = np.random.rand(outputNeuronsCount, 1)
+        self.h1Weights = np.random.rand(h1NeuronCount, inputNeuronCount)
+        self.h1Biases = np.random.rand(h1NeuronCount, 1)
+        self.h2Weights = np.random.rand(h2NeuronCount, h1NeuronCount)
+        self.h2Biases = np.random.rand(h2NeuronCount, 1)
+        self.outputWeights = np.random.rand(outputNeuronCount, h2NeuronCount)
+        self.outputBiases = np.random.rand(outputNeuronCount, 1)
 
-        self.inputActivations = np.zeros((inputNeuronsCount, 1), order=MATRIX_MODE)
-        self.hidden1Activations = np.zeros((hiddenNeurons1Count, 1), order=MATRIX_MODE)
-        self.hidden2Activations = np.zeros((hiddenNeurons2Count, 1), order=MATRIX_MODE)
-        self.outputActivations = np.zeros((outputNeuronsCount, 1), order=MATRIX_MODE)
+        #Keep initialized to save time
+        self.inputActivations = np.zeros((inputNeuronCount, 1), order=MATRIX_MODE)
+        self.h1Activations = np.zeros((h1NeuronCount, 1), order=MATRIX_MODE)
+        self.h2Activations = np.zeros((h2NeuronCount, 1), order=MATRIX_MODE)
+        self.outputActivations = np.zeros((outputNeuronCount, 1), order=MATRIX_MODE)
+
+        self.outputTarget = np.zeros((outputNeuronCount, 1), order=MATRIX_MODE)
+
+        self.h1Error = np.zeros((inputNeuronCount, 1), order=MATRIX_MODE)
+        self.h2Error = np.zeros((inputNeuronCount, 1), order=MATRIX_MODE)
+        self.outputError = np.zeros((inputNeuronCount, 1), order=MATRIX_MODE)
+
+        #Keep for backprop
+        self.h1Raw = self.h1Activations
+        self.h2Raw = self.h2Activations
+        self.outputRaw = self.outputActivations
         print("Initialized NeuralNetwork4!")
 
     def feedforward(self, input):
         """Accepts a vector of input neuron values and returns a vector of output neuron values."""
-        if len(input) != self.inputNeuronsCount:
-            raise InputSizeException(input, self.inputNeuronsCount) #can remove this later to speed up
-        self.hidden1Activations = sigmoid((self.h1Weights @ input) + self.h1Biases)
-        self.hidden2Activations = sigmoid((self.h2Weights @ self.hidden1Activations) + self.h2Biases)
-        self.outputActivations = sigmoid((self.outputWeights @ self.hidden2Activations) + self.outputBiases)
+        if len(input) != self.inputNeuronCount:
+            raise InputSizeException(input, self.inputNeuronCount) #can remove this later to speed up
+        self.inputActivations = input
+
+        #Calculate the first layer
+        self.h1Raw = (self.h1Weights @ self.inputActivations) + self.h1Biases
+        self.h1Activations = sigmoid(self.h1Raw)
+
+        #Calculate the second layer
+        self.h2Raw = (self.h2Weights @ self.h1Activations) + self.h2Biases
+        self.h2Activations = sigmoid(self.h2Raw)
+
+        #Calculate the output layer
+        self.outputRaw = (self.outputWeights @ self.h2Activations) + self.outputBiases
+        self.outputActivations = sigmoid(self.outputRaw)
+
         return self.outputActivations
 
-start_time = time.time()
+    def backpropagateError(self):
+        """Computes each layer's error vector via backpropagation."""
+        self.outputError = np.concatenate(2*(self.outputActivations-self.outputTarget)*dSigmoid(self.outputRaw)).transpose()
 
-testNetwork = NeuralNetwork4(3000, 40, 30, 10)
+        self.h2Error = np.concatenate((self.outputWeights.transpose() @ self.outputError).reshape(self.h2NeuronCount, 1) * dSigmoid(self.h2Raw).reshape(self.h2NeuronCount, 1))
+        self.h1Error = np.concatenate((self.h2Weights.transpose() @ self.h2Error).reshape(self.h1NeuronCount, 1) * dSigmoid(self.h1Raw).reshape(self.h1NeuronCount, 1))
 
-for i in range(10000):
-    (testNetwork.feedforward(np.random.rand(3000, 1)))
+    
 
-print("--- %s seconds ---" % (time.time() - start_time))
+#start_time = time.time()
+testNetwork = NeuralNetwork4(5, 6, 7, 8)
+testNetwork.feedforward(np.random.rand(5, 1))
+testNetwork.outputTarget = np.array([0, 1, 0, 0, 0, 0, 0, 0], ndmin=2).transpose()
+testNetwork.backpropagateError()
+print(f"Output EV: {testNetwork.outputError}")
+print(f"Hidden2 EV: {testNetwork.h2Error}")
+print(f"Hidden1 EV: {testNetwork.h1Error}")
+# for i in range(10000):
+#     (testNetwork.feedforward(np.random.rand(3000, 1)))
+
+# print("--- %s seconds ---" % (time.time() - start_time))
 
 
