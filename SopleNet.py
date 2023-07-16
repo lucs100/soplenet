135a62ba72575817987c182dd0cfe5ca882f7f45
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2023-07-16 01:01:19 -0400

    Implement wt/bias tuning functions The network has successfully trained! :D

--- SopleNet.py
+++ SopleNet.py
@@ -26,8 +26,10 @@ class NeuralNetwork4: #4-layer neural network
 
         self.h1Weights = rng.random((h1NeuronCount, inputNeuronCount))*2-1
         self.h1Biases = rng.random((h1NeuronCount,))*2-1
+
         self.h2Weights = rng.random((h2NeuronCount, h1NeuronCount))*2-1
         self.h2Biases = rng.random((h2NeuronCount,))*2-1
+
         self.outputWeights = rng.random((outputNeuronCount, h2NeuronCount))*2-1
         self.outputBiases = rng.random((outputNeuronCount,))*2-1
 
@@ -53,9 +55,10 @@ class NeuralNetwork4: #4-layer neural network
         """Accepts a vector of input neuron values and returns a vector of output neuron values."""
         if len(input) != self.inputNeuronCount:
             raise InputSizeException(input, self.inputNeuronCount) #can remove this later to speed up
+        self.inputActivations = input
 
         #Calculate the first layer
-        self.h1Raw = (self.h1Weights @ input) + self.h1Biases
+        self.h1Raw = (self.h1Weights @ self.inputActivations) + self.h1Biases
         self.h1Activations = sigmoid(self.h1Raw)
 
         #Calculate the second layer
@@ -85,27 +88,49 @@ class NeuralNetwork4: #4-layer neural network
         biasGradientH2 = self.h2Error * self.h2Biases
         biasGradientOutput = self.outputError * self.outputBiases
         return [biasGradientH1, biasGradientH2, biasGradientOutput]
+    
+    def getWeightGradient(self):
+        """Calculates and returns 3 gradient-like matrices for the weights in the network."""
+        weightGradientH1 = self.inputActivations * self.h1Error[:, np.newaxis]  #Equivalent of transpose, creates a meshgrid-like sum.
+        weightGradientH2 = self.h1Activations * self.h2Error[:, np.newaxis]
+        weightGradientOutput = self.h2Activations * self.outputError[:, np.newaxis]
+        return [weightGradientH1, weightGradientH2, weightGradientOutput]
 
     def adjustBiases(self, gradient, eta):
-        """Adjusts the biases given some gradient-like vectors of the weights and some training rate eta. Gradient is a list of 4 vectors."""
-        raise NotImplementedError
+        """Adjusts the biases given some gradient-like vectors of the biases and some training rate eta. Gradient is a list of 3 vectors."""
+        biasGradientH1, biasGradientH2, biasGradientOutput = gradient
+        self.h1Biases -= (biasGradientH1)*eta
+        self.h2Biases -= (biasGradientH2)*eta
+        self.outputBiases -= (biasGradientOutput)*eta
+
+    def adjustWeights(self, gradient, eta):
+        """Adjusts the weights given some gradient-like matrices of the weights and some training rate eta. Gradient is a list of 3 matrices."""
+        weightGradientH1, weightGradientH2, weightGradientOutput = gradient
+        self.h1Weights -= (weightGradientH1)*eta
+        self.h2Weights -= (weightGradientH2)*eta
+        self.outputWeights -= (weightGradientOutput)*eta
     
 
 
 #start_time = time.time()
 testNetwork = NeuralNetwork4(5, 6, 7, 8)
-testNetwork.feedforward(np.random.rand(5,))
 testNetwork.outputTarget = np.array([0, 1, 0, 0, 0, 0, 0, 0])
-testNetwork.backpropagateError()
-print(f"Output EVec: {testNetwork.outputError}")
-print(f"Hidden2 EVec: {testNetwork.h2Error}")
-print(f"Hidden1 EVec: {testNetwork.h1Error}")
+# print(f"Output EVec: {testNetwork.outputError}")
+# print(f"Hidden2 EVec: {testNetwork.h2Error}")
+# print(f"Hidden1 EVec: {testNetwork.h1Error}")
 
-testNetwork.getBiasGradient()
+#Example training:
+for i in range(1000):
+    testNetwork.feedforward(np.linspace(0.1, 0.9, 5))
+    testNetwork.backpropagateError()
 
-print(testNetwork.getIterationCost())
-# for i in range(10000):
-#     (testNetwork.feedforward(np.random.rand(3000, 1)))
+    biasGradient = testNetwork.getBiasGradient()
+    weightGradient = testNetwork.getWeightGradient()
+
+    testNetwork.adjustBiases(biasGradient, eta=0.01)
+    testNetwork.adjustWeights(weightGradient, eta=0.01)
+
+    print(f"Current cost: {testNetwork.getIterationCost()} \t {testNetwork.outputActivations}")
 
 # print("--- %s seconds ---" % (time.time() - start_time))
 
